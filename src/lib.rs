499fd8ce7906c93c1c7be3834a90eebd59977309
Maurice Berk <maurice@mauriceberk.com>
2023-08-11 16:12:56 +0100

    Add docstring

--- src/lib.rs
+++ src/lib.rs
@@ -196,6 +196,19 @@ fn calculate_log_expected_wealth(
     expected_log_wealth
 }
 
+///     Calculate the optimal fractional Kelly stake when placing a single bet on an event that has one winner
+///
+///     :param price: The price at which the bet is being placed
+///     :param is_back: Whether you are backing (True) or laying (False)
+///     :param probability: Your fair probability for the outcome you are betting on
+///     :param other_probabilities: A list of probabilities for the other possible outcomes. sum(other_probabilities) + probability should be equal to 1 subject to floating-point error but this is not yet enforced
+///     :param position: Your position - i.e. how much you are currently standing to win or lose - on the outcome you are betting on
+///     :param other_positions: A list of positions corresponding to the other possible outcomes. It is assumed there is a 1:1 correspondence between the elements of this list and other_probabilities
+///     :param bankroll: Your notional Kelly bankroll
+///     :param kelly_fraction: A fraction to multiply the optimal stake by. Defaults to 1
+///     :param verbose: Whether to generate log statements when numerically optimising the stake. Defaults to False
+///
+///     :return: The optimal fractional Kelly stake
 #[pyfunction]
 #[pyo3(signature = (price, is_back, probability, other_probabilities, position, other_positions, bankroll, kelly_fraction = 1.0, verbose = false))]
 fn calculate_kelly_stake(
